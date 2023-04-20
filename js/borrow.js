e5cba9a2211e9cf7ec9b902cf37c6aff4c087fd4
simon TAPIAU <simtapiau@live.fr>
2023-04-20 17:00:50 +0200

    rendu

--- js/borrow.js
+++ js/borrow.js
@@ -3,12 +3,13 @@ const borrowstart=document.querySelector("#start");
 const borrowend=document.querySelector("#return");
 const valid=document.querySelector("#validation");
 const selectalert=document.querySelector("#selectalert");
-const datealert=document.querySelector('#timealert')
+const datealert=document.querySelector('#timealert');
+const toofaralert=document.querySelector('#toofaralert');
 const startdefaultstyle=borrowstart.style;
 const enddefaultstyle=borrowend.style;
 const gamedropdowndefaultstyle=gamedropdown.style;
 
-fetch('https://api.npoint.io/00f2a23cb6d801c23643').
+fetch('https://api.npoint.io/33fe536f3a3bc2f018fb').
         then(result=>result.json()).
         then(json=>json.games.forEach(jeu => {
             gamedropdown.insertAdjacentHTML( 'beforeend',`<option value=${jeu.title}'>${jeu.title}</option>`);
@@ -18,12 +19,12 @@ fetch('https://api.npoint.io/00f2a23cb6d801c23643').
 
 valid.addEventListener("click",click=>{
     click.preventDefault();
+    const ONE_YEAR_IN_MILLISECONDS = 31536000000;
     let success=true;
     const startddate=new Date(borrowstart.value);
     const enddate=new Date(borrowend.value);
-    console.log(Date.parse(startddate));
-    console.log(Date.parse(enddate));
-    datevalide=(Date.parse(startddate)-Date.parse(enddate)<0)
+    datevalide=(Date.parse(startddate)-Date.parse(enddate)<0);
+    toofar=enddate-startddate<ONE_YEAR_IN_MILLISECONDS;
     if(gamedropdown.value===""){
         success=false;
         selectalert.style.display="block";
@@ -35,6 +36,18 @@ valid.addEventListener("click",click=>{
         borrowend.style.border="1px solid red";
         datealert.style.display="block";
     }
+    else{
+        success=true;
+        borrowstart.style.border=startdefaultstyle;
+        borrowend.style.border=enddefaultstyle;
+        datealert.style.display="none";
+    }
+    if(!toofar){
+        success=false;
+        borrowstart.style.border="1px solid red";
+        borrowend.style.border="1px solid red";
+        toofaralert.style.display="block";
+    }
     if(success){
         alert("Jeu loué avec succés");
         borrowstart.style=startdefaultstyle;
@@ -42,5 +55,6 @@ valid.addEventListener("click",click=>{
         gamedropdown.style=gamedropdowndefaultstyle;
         selectalert.style.display="none";
         datealert.style.display="none";
+        toofaralert.style.display="none";
     }
-})
+});
